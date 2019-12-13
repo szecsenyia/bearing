9a96609391cc1c751ab62f9a57d3e2611249d404
aszecsenyi <akos.szecsenyi@vertis.com>
2019-12-13 17:34:23 +0100

    Phase one

--- app/app/urls.py
+++ app/app/urls.py
@@ -16,6 +16,9 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+from bears.views import home_view
+
 urlpatterns = [
+    path('home/', home_view, name='home'),
     path('admin/', admin.site.urls),
 ]
--- app/bearing/models.py
+++ app/bearing/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
+
 # Create your models here.
+class Product(models.Model):
+    title = models.CharField(blank=True, max_length=120)
+    description = models.TextField(blank=True, null=True)
+    price = models.DecimalField(decimal_places=2, max_digits=1000)
+    ingredients = models.TextField(default = 'blank')
+    available = models.BooleanField(default = 'True')
